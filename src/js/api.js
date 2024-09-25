4470e76933210aa8a31c943dd5433b21605c3245
Rhoglas Aragão <96073350+Rhoglas@users.noreply.github.com>
2024-09-25 19:15:57 -0300

    Atualização na experiencia

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,79 @@
 // lista de certificados
 export const licence = [
   {
+    title: "Gestão Administrativa",
+    date: "2024",
+    desc: `Participei de um curso de Gestão Administrativa com carga horária de 100 horas, onde aprendi habilidades e conhecimentos fundamentais para atuar de forma eficaz na área administrativa de uma empresa.
+
+Durante o curso, estudei os princípios da gestão organizacional, aprendendo sobre estrutura organizacional, planejamento estratégico e a importância da definição de metas. Aprendi a elaborar e implementar processos administrativos que ajudam a otimizar a eficiência da equipe e dos recursos disponíveis.
+
+O curso também abordou temas como gestão de pessoas, onde aprendi sobre recrutamento, seleção, treinamento e desenvolvimento de colaboradores, além de como motivar e engajar a equipe para alcançar melhores resultados. Estudei a importância da comunicação interna e como ela impacta o clima organizacional.
+
+Outra parte importante do curso foi a gestão financeira, onde adquiri conhecimentos sobre elaboração de orçamentos, controle de custos e análise de demonstrações financeiras, essenciais para a tomada de decisões informadas.`,
+  },
+  {
+    title: "Analista financeiro",
+    date: "2024",
+    desc: `Participei de um curso de Analista Financeiro com carga horária de 100 horas, onde adquiri conhecimentos fundamentais para atuar na área financeira de empresas.
+
+Durante o curso, aprendi a interpretar e analisar demonstrações financeiras, como balanço patrimonial, DRE (Demonstração do Resultado do Exercício) e fluxo de caixa. Entendi como essas ferramentas são essenciais para avaliar a saúde financeira de uma empresa e auxiliar na tomada de decisões.
+
+Também estudei conceitos de análise de investimentos, como cálculo de indicadores financeiros (ROE, ROI, margem de lucro), além de ferramentas de planejamento financeiro e projeção de resultados. O curso me ensinou a utilizar o Excel para criar relatórios financeiros e automatizar cálculos, facilitando o controle de orçamentos e o acompanhamento de receitas e despesas.
+
+Outro ponto importante foi o estudo de finanças corporativas, onde aprendi sobre estrutura de capital, avaliação de riscos e estratégias para maximizar o valor da empresa. O curso também abordou técnicas de controladoria e gestão de custos, essenciais para a eficiência financeira de uma organização.
+
+Ao final do curso, obtive uma visão abrangente sobre como gerenciar e analisar a parte financeira de uma empresa, utilizando ferramentas e técnicas modernas para apoiar o crescimento e a sustentabilidade dos negócios.`,
+  },
+  {
+    title: "Auxiliar Administrativo",
+    date: "2024",
+    desc: `Participei de um curso de Auxiliar Administrativo com carga horária de 100 horas, onde aprendi habilidades essenciais para atuar na área administrativa de uma empresa.
+
+Durante o curso, estudei os fundamentos da gestão administrativa, incluindo o funcionamento de um escritório, organização de documentos e rotinas de trabalho. Aprendi a utilizar ferramentas de Microsoft Office, como Word para redação de documentos, Excel para gerenciamento de dados e PowerPoint para criação de apresentações.
+
+O curso também abordou a importância da comunicação eficaz no ambiente de trabalho, ensinando técnicas de atendimento ao cliente e como elaborar relatórios e e-mails profissionais. Aprendi sobre gestão de tempo e como priorizar tarefas, o que é fundamental para aumentar a produtividade.
+
+Além disso, estudei conceitos de financeiras e contabilidade básica, como controle de despesas, emissão de notas fiscais e organização de contas a pagar e a receber. Também tive noções sobre legislação trabalhista e a importância da ética profissional no ambiente administrativo.
+
+Ao final do curso, me senti preparado para desempenhar funções administrativas, contribuindo para a organização e eficiência de processos dentro de uma empresa.`,
+  },
+
+  {
+    title: "Git e Github",
+    date: "2023",
+    desc: `Participei de um curso de Git e GitHub que me ensinou a gerenciar versões de projetos e 
+    colaborar de forma eficiente com equipes de desenvolvimento.
+    No curso, aprendi o básico do Git, como criar e gerenciar repositórios locais, rastrear alterações em 
+    arquivos, e utilizar comandos essenciais como git add, git commit, git push e git pull. Também entendi 
+    como usar ramificações (branches) para trabalhar em diferentes versões de um projeto simultaneamente, 
+    o que facilita a organização e o controle de mudanças.
+    Além disso, aprendi a utilizar o GitHub, uma plataforma de hospedagem de repositórios Git. Aprendi a
+    criar repositórios remotos, clonar projetos, e colaborar com outras pessoas através de pull requests, 
+    revisão de código, e resolução de conflitos. O curso também abordou como utilizar forks para contribuir
+    em projetos de terceiros e a importância do versionamento no trabalho em equipe.
+    Ao final, entendi como o Git e o GitHub são fundamentais para o controle de versões em projetos de 
+    software, além de serem ferramentas indispensáveis para colaboração em equipe, organização e manutenção
+    de código de forma eficiente e segura.`,
+  },
+  {
+    title: "Start na programação",
+    date: "2023",
+    desc: `Participei do curso Start na Programação, onde aprendi os fundamentos essenciais para criar
+     sites e hospedar projetos na web.
+No curso, comecei com HTML, aprendendo a estruturar páginas da web, utilizando tags para organizar 
+conteúdo como textos, links, imagens e listas. Em seguida, explorei o CSS, que me permitiu estilizar 
+essas páginas, definindo cores, fontes, margens, e layouts, tornando o visual das páginas mais atraente
+ e profissional.
+Aprendi também JavaScript (JS), que trouxe interatividade para os sites. Com ele, pude criar animações,
+ validar formulários, e responder a eventos de usuários, como cliques e envio de dados, dando mais 
+ dinamismo às páginas.
+Por fim, aprendi a hospedar sites no GitHub, utilizando o GitHub Pages para publicar projetos online. 
+Isso me permitiu não só versionar meus arquivos com Git, mas também colocar meus projetos no ar para 
+que qualquer pessoa pudesse acessá-los pela web.
+Esse curso me deu uma base sólida para começar a desenvolver sites completos e colocá-los online de 
+forma prática.`,
+  },
+  {
     title: "Full Stack JavaScript",
     date: "2023 - Atualmente",
     desc: `O curso Full Stack JavaScript é uma jornada completa 
@@ -14,6 +87,7 @@ export const licence = [
     inclui exercícios, projetos e provas, além de suporte e 
     comunidade exclusivos`,
   },
+
   {
     title: "JavaScript Lucrativo",
     date: "2023 - Atualmente",
@@ -24,6 +98,61 @@ export const licence = [
     freelancer de sucesso, precificação de sistemas, faturando como
     fleelancer e projetos paralelos aos tópicos de 
     estudo.`,
+  },
+
+  {
+    title: "Informática básica e avançada",
+    date: "2020",
+    desc: `Participei do curso de Informática Básica e Avançada,
+    Na parte básica, aprendi o funcionamento dos computadores, como organizar arquivos no Windows, 
+    e usei o Pacote Office, principalmente o Word para textos, o Excel para planilhas simples, e o 
+    PowerPoint para apresentações. Também aprendi a navegar na internet de forma segura, gerenciar 
+    emails e utilizar redes sociais de maneira eficiente.
+    Na parte avançada, aprofundei meu conhecimento no Excel, aprendendo a usar fórmulas complexas, gráficos 
+    e tabelas dinâmicas. Também aprendi noções de manutenção de computadores, como diagnosticar problemas 
+    e melhorar o desempenho. O curso incluiu ainda conceitos de redes de computadores e segurança digital, 
+    ensinando a proteger dados e a evitar fraudes. Por fim, tive uma introdução à programação e automação 
+    no Excel com VBA.
+    Foi um curso completo que me preparou para usar a informática de maneira mais prática e eficiente no 
+    trabalho e no dia a dia.
+     `,
+  },
+  {
+    title: "Eletromecânica",
+    date: "2020",
+    desc: `Este curso foi realizado no período de 14 de outubro de 2019 a 18 de dezembro de 2020,
+     totalizando uma formação completa e diversificada em áreas essenciais de elétrica e mecânica.
+     
+     Na parte de Instalação Elétrica Predial, com uma carga horária de 400 horas, aprendi a realizar
+      a instalação de lâmpadas comandadas por interruptores simples, paralelos e intermediários; 
+      instalação de fotocélulas, sensores de presença e tomadas; além de comandos automáticos como 
+      partida direta simples para motor trifásico com sinalizadores, partida estrela-triângulo com 
+      botoeiras e sinalizadores, e partida direta com relé temporizado e relé falta de fase em motor trifásico.
+
+      Na parte de Formação Profissional Mecânica, com carga horária de 300 horas, estudei metrologia para
+      mecânico, leitura de serviços, desenho técnico, uso de catálogos de peças, manuais de serviços, hidráulica,
+      material rodante, motor de combustão interna, sistemas de freios, direção e lubrificação. Também aprendi 
+      sobre processos de soldagem a gás (OFW), soldagem com eletrodo revestido e elaboração de procedimentos de
+      soldagem (EPS).`,
+  },
+
+  {
+    title: "Elétrica",
+    date: "2020",
+    desc: `Participei do curso de Eletricista completo no período 
+    de 22 de novembro a 13 de dezembro de 2020, com carga horária de 100 horas. 
+    Durante o curso, estudei os seguintes conteúdos:
+      CONTEÚDO PROGRAMÁTICO: ✓ Historicidade da Eletricidade;
+      ✓ Eletromagnetismo;
+      ✓ Caminhos da Eletricidade;
+      ✓ Circuito Elétrico;
+      ✓ Sistema Internacional de Unidades;
+      ✓ Transmissão de energia elétrica;
+      ✓ Padrão de entrada de circuitos monofásicos e trifásicos;
+      ✓ Ferramentas de Atuação do Eletricista;
+      ✓ Conceitos Básicos de Magnetismo;
+      ✓ Sistemas de para-raios;
+      ✓ Sistema Elétrico de Potência - SEP.`,
   },
 ];
 
@@ -41,6 +170,22 @@ export const experience = [
     eficiência operacional, e elaborava relatórios detalhados 
     sobre o rendimento diário e mensal, destacando os lucros 
     obtidos pela instituição laboratorial.`,
+  },
+  {
+    title: "Auxiliar de TI -- AUTÔNOMO",
+    date: "2022 -- ATUALMENTE",
+    desc: `
+    Atualmente, trabalho como Auxiliar de TI Autônomo, onde desempenho uma série de funções essenciais para garantir o funcionamento eficiente de sistemas e tecnologia em empresas e para clientes individuais.
+
+Uma das minhas principais responsabilidades é realizar suporte técnico, auxiliando usuários a resolver problemas relacionados a hardware e software. Isso envolve desde a instalação e configuração de equipamentos até a solução de questões como falhas no sistema, conectividade de rede e dificuldades com aplicativos.
+
+Além disso, sou responsável pela manutenção preventiva e corretiva de computadores e dispositivos, garantindo que todos os equipamentos estejam em bom estado de funcionamento. Isso inclui a limpeza de componentes, atualização de software e verificação de segurança para evitar vírus e outras ameaças.
+
+Outra parte importante do meu trabalho é a gestão de redes, onde ajudo a configurar redes locais, roteadores e dispositivos de segurança, além de monitorar o desempenho da rede para garantir a conectividade e a segurança dos dados.
+
+Além do suporte técnico, também ofereço serviços de consultoria em tecnologia da informação, orientando os clientes sobre as melhores práticas, ferramentas e soluções que podem atender às suas necessidades. Isso pode incluir recomendações sobre software de produtividade, ferramentas de colaboração e estratégias de segurança digital.
+
+Por fim, mantenho-me atualizado sobre as tendências e inovações em tecnologia, o que me permite oferecer soluções atuais e eficientes aos meus clientes. Como Auxiliar de TI Autônomo, minha missão é garantir que a tecnologia funcione de forma otimizada, proporcionando suporte e soluções que facilitem o dia a dia dos usuários.`,
   },
 ];
 
